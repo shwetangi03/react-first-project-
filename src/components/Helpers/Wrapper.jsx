503ef64e7d49b1908ff904aa500e22ef95aa9845
Shwetangi <shwetangichaurasia4@gmail.com>
2024-03-23 09:28:36 +0530

    Initial commit

---
+++ src/components/Helpers/Wrapper.jsx
@@ -0,0 +1,10 @@
+const Wrapper =(props)=>{
+    return (
+        <React.Fragment>
+            <h2>hey there </h2>
+            <p>this IS FRAGMENGT</p>
+        </React.Fragment>
+    )
+}
+
+export default Wrapper;
